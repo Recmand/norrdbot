5353799c0193bfdabb2c260d6fd9cfc129ea677b
Glitch (able-rightful-specialist) <none>
2020-11-26 03:56:39 +0000

    🐘🍦 Checkpoint ./commands/play.js:23794493/30 ./commands/nowplaying.js:23794493/16 ./server.js:23794493/41

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -6,6 +6,6 @@ module.exports = {
      const serverQueue = client.queue.get(message.guild.id);
     if (!serverQueue) return message.reply("There is nothing playing.").catch(console.error);
     const song = serverQueue.songs[0]
-      message.channel.send(`Now Playing: ${song.title} from ${song.channel}. Requested by <@${song.playUser}>`)
+      message.channel.send(`:arrow_forward: Now Playing: ${song.title} from ${song.channel}. Requested by <@${song.playUser}>`)
   }
 };
